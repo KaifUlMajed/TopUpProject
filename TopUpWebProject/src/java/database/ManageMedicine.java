f56ed2f69dceb9120a8d190e9bb28387981287cb
KaifUlMajed <mizanshaity@gmail.com>
2018-03-29 16:09:06 +0600

    Added the medicine control methods for admin

---
+++ TopUpWebProject/src/java/database/ManageMedicine.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+/**
+ *
+ * @author Kaif Ul Majed
+ */
+import dao.Medicine;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ManageMedicine {
+    private static SessionFactory factory;
+    public ManageMedicine() {
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public Integer addMedicine(Medicine m){
+
+        Integer id = null;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            id = (Integer)session.save(m);
+            tx.commit();
+            
+        }catch(HibernateException he){
+            if (tx!=null) tx.rollback();
+            he.printStackTrace();
+        }
+        finally{
+            session.close();
+        }
+        return id;        
+    }
+    
+    public List<Medicine> getMedByType(String type){
+        List <Medicine> meds = null;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            meds = session.createQuery("FROM Medicine M WHERE M.type=" + type).list();
+            tx.commit();
+        }catch(HibernateException he){
+            if (tx!=null) tx.rollback();
+            he.printStackTrace();
+        }
+        finally{
+            session.close();
+        }        
+        return meds;
+    }
+    
+    public List<Medicine> getMedByGenericName(String gn){
+        List <Medicine> meds = null;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            meds = session.createQuery("FROM Medicine M WHERE M.generic_name=" + gn).list();
+            tx.commit();
+        }catch(HibernateException he){
+            if (tx!=null) tx.rollback();
+            he.printStackTrace();
+        }
+        finally{
+            session.close();
+        }        
+        return meds;
+    }
+    public List<Medicine> getMedByName(String name){
+        List <Medicine> meds = null;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            meds = session.createQuery("FROM Medicine M WHERE M.name=" + name).list();
+            tx.commit();
+        }catch(HibernateException he){
+            if (tx!=null) tx.rollback();
+            he.printStackTrace();
+        }
+        finally{
+            session.close();
+        }        
+        return meds;
+    }     
+    public List<Medicine> getAllMeds(){
+        List <Medicine> meds = null;
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            meds = session.createQuery("FROM Medicine").list();
+            tx.commit();
+        }catch(HibernateException he){
+            if (tx!=null) tx.rollback();
+            he.printStackTrace();
+        }
+        finally{
+            session.close();
+        }        
+        return meds;
+    }     
+    
+    
+}
